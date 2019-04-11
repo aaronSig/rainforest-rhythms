be3fe0804f1135b4a1caf50786b386fdca03e214
Aaron Signorelli <aarons88@gmail.com>
2019-04-11 08:35:57 +0100

    better sizing on mobile

--- src/components/Article/ArticleFooter.tsx
+++ src/components/Article/ArticleFooter.tsx
@@ -5,12 +5,14 @@ import UP from "./up.png";
 function ArticleFooter() {
   return (
     <footer className={styles.ArticleFooter}>
-      <span>
-        <a href="https://github.com/aaronSig/rainforest-rhythms">This website is open source</a>
-      </span>
-      <a href="https://weareup.co">
-        <img alt="UP Logo" src={UP} />
-      </a>
+      <div className={styles.Inner}>
+        <span>
+          <a href="https://github.com/aaronSig/rainforest-rhythms">This website is open source</a>
+        </span>
+        <a href="https://weareup.co">
+          <img alt="UP Logo" src={UP} />
+        </a>
+      </div>
     </footer>
   );
 }
